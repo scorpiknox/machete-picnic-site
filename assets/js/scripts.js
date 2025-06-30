accbdb1a70d59dc28a66bfe1edee74e75c07b58c
scorpiknox <dsummers.iit@gmail.com>
2025-06-30 12:35:17 -0700

    Commited from pc

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollContainer = document.querySelector(".scroll-container");
+  const scrollLeftBtn = document.querySelector(".scroll-left");
+  const scrollRightBtn = document.querySelector(".scroll-right");
 
+  if (scrollContainer && scrollLeftBtn && scrollRightBtn) {
+    scrollLeftBtn.addEventListener("click", () => {
+      scrollContainer.scrollBy({ left: -300, behavior: "smooth" });
+    });
+
+    scrollRightBtn.addEventListener("click", () => {
+      scrollContainer.scrollBy({ left: 300, behavior: "smooth" });
+    });
+  }
+});
